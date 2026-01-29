8dc0e643ca5c47c1d7699e141e3e50e2d1dc3508
rajesh maddula <rajesh.maddula21@gmail.com>
2026-01-29 18:26:34 +0530

    added something

--- 03.variables.sh
+++ 03.variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Suresh
-PERSON2=Ramesh
+PERSON1=Trump
+PERSON2=Putin
 echo "$PERSON1 :: Hello Ramesh, Hoe are you"
 echo "$PERSON2 :: Hi Suresh, I am fine thanks for asking. Hoe are you doing?"
 echo "$PERSON1 :: I am learning shell. Waht about you?"
