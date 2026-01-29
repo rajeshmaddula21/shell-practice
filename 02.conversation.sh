13a0d10eb1e5c97637e5a4fd700f95fa790155c0
rajesh maddula <rajesh.maddula21@gmail.com>
2026-01-29 18:18:33 +0530

    added something

--- 02.conversation.sh
+++ 02.conversation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "Suresh :: Hello Ramesh, Hoe are you"
-echo "Ramesh :: Hi Suresh, I am fine thanks for asking. Hoe are you doing?"
-echo "Suresh :: I am learning shell. Waht about you?"
+PERSON1=Suresh
+PERSON2=RAMESH
+echo "$PERSON1 :: Hello Ramesh, Hoe are you"
+echo "$PERSON2 :: Hi Suresh, I am fine thanks for asking. Hoe are you doing?"
+echo "$PERSON1 :: I am learning shell. Waht about you?"
