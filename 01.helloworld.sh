36ec259a6548faeeda884788af0504c65951e632
rajesh maddula <rajesh.maddula21@gmail.com>
2026-01-29 18:21:23 +0530

    added something

--- 01.helloworld.sh
+++ 01.helloworld.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-
-echo "hello world"
-echo "Hi, I am learning GIT"
+ERSON1=Suresh
+PERSON2=RAMESH
+echo "$PERSON1 :: Hello Ramesh, Hoe are you"
+echo "$PERSON2 :: Hi Suresh, I am fine thanks for asking. Hoe are you doing?"
+echo "$PERSON1 :: I am learning shell. Waht about you?"
