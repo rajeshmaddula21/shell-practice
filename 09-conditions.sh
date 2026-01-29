8dd7b7d8bcc3b3b2397cac85df221f29019897ac
rajesh maddula <rajesh.maddula21@gmail.com>
2026-01-29 20:34:30 +0530

    added something

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -7,7 +7,7 @@ NUMBER=$1
 # -eq -> equal
 # -ne -> not eqal
 
-if [ $NUMBER -gt 20]; then
+if [ $NUMBER -gt 20 ]; then
    echo "Given number: $NUMBER is greater than 20"
 else
    echo "Given number: $NUMBER is less than 20"
