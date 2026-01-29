e890d9a9e3a4bd05cedccb7ce33b93001b994c55
rajesh maddula <rajesh.maddula21@gmail.com>
2026-01-29 19:51:54 +0530

    added something

---
+++ 07.variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Special variables ##
+
+echo "All args passed to the script:  $@"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directoty of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 $
+echo "PID of recently excuted bacgroung process: $!"
+echo "All args passedd to script: $*"
