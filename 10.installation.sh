fff02f29b6aa0e9847e9e030de9949bc6e883b3e
rajesh maddula <rajesh.maddula21@gmail.com>
2026-01-30 12:42:10 +0530

    added new file

--- 10.installation.sh
+++ 10.installation.sh
@@ -5,7 +5,14 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ] ; then
     echo "Please run this script with root access"
     exit 1
-
 fi
+
 echo "Installing Nginx"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx ...Failur"
+    exit 1
+else 
+    echo "Installing  Nginx ...Success"
+fi
