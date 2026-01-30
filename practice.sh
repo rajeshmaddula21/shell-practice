fb4013a146a38dd7add1b37a368d519f51bd48dc
rajesh maddula <rajesh.maddula21@gmail.com>
2026-01-30 19:41:23 +0530

    added new file

--- practice.sh
+++ practice.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 
-UDERSID=$(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
 
 echo "Please run the script with the root user"
 
 dnf install nginx -y
-echo "Installing Ngix"
+echo "Installing Nginx"
 
 fi
 
