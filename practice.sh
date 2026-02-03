24c70db707ca192f2da736ce86537102557e24a6
rajesh maddula <rajesh.maddula21@gmail.com>
2026-02-03 22:19:29 +0530

    update

--- practice.sh
+++ practice.sh
@@ -3,14 +3,14 @@
 USERID=$(id -u)
 
 
-if [ $USERID -eq 0]; then
+if [ $USERID -eq 0 ]; then
 echo "Please run the script with Root User"
 exit 1
 
 fi
 
 VALIDATE() {
-    if [ $1 -ne 0];then
+    if [ $1 -ne 0 ];then
 echo "$2 ..Failur"
 exit 1
 else
@@ -18,7 +18,7 @@ echo "$2 ..Success"
 
 fi
 }
-dnf install Nginx -y
+dnf install nginx -y
 VALIDATE $? "Installing Nginx" 
 
 
