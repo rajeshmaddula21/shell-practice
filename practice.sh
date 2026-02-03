ffe131087839e73d5f46b21020bed0641996bf3d
rajesh maddula <rajesh.maddula21@gmail.com>
2026-02-03 22:12:38 +0530

    update

--- practice.sh
+++ practice.sh
@@ -2,19 +2,24 @@
 
 USERID=$(id -u)
 
+
 if [ $USERID -eq 0]; then
 echo "Please run the script with Root User"
 exit 1
 
 fi
 
-echo "Installing Nginx"
-dnf install mongodb -y
-
-if [ $? -ne 0];then
+VALIDATE() {
+    if [ $1 -ne 0];then
 echo "Intalling Nginx ..Failur"
 exit 1
 else
-echo "Installing Nginx ..Successghhh"
+echo "Installing Nginx ..Success"
 
 fi
+}
+dnf install mongodb -y
+VALIDATE $1 
+
+
+
