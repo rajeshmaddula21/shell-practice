9aae23ee36539628f967f0f89af67bf4cc6bc3fd
rajesh maddula <rajesh.maddula21@gmail.com>
2026-02-06 01:22:34 +0530

    new file or updated

---
+++ 19.trap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e # ERR
+
+#trap 'echo, "There an erro in $LINENO, Command: $BASH_COMMAND"' ERR
+
+echo "Hello world"
+echo "I am learning shell script"
+echoo "Printing error here"
+echo "No error in this"
